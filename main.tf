13cdbc7447690f3723fdcce1f2f1498001e2172c
BasharNaieem <109580549+BasharNaieem@users.noreply.github.com>
2024-09-02 15:44:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ module "createInstanceAws" {
   key_name            = var.key_name
   security_group_name = var.security_group_name
   user_data           = var.user_data
-  instance_name       = var.instance_name
+  instance_name_jenkins       = var.instance_name
 }
